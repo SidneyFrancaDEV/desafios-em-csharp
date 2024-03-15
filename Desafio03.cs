fd9c413e2dd602db4287d200e624a681b564cc01
Sidney França <142261721+SidneyFrancaDEV@users.noreply.github.com>
2024-03-15 14:23:25 -0300

    Add files via upload

---
+++ Desafio03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+   static void Main()
+    {
+
+        Console.WriteLine("Este programa vai te mostrar os numeros da sequencia de Fibonnaci. Digite a quantidade de numeros para a sequência:");
+
+        int fstN = 0;
+        
+        int sndN = 1;
+
+        int UserRequest = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < UserRequest; i++)
+        {
+            Console.Write($" {fstN}");
+            sndN = fstN + sndN;
+            fstN = sndN - fstN;
+        }
+
+    }
+}
